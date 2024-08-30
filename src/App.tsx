f2545505f1218ad8a70cd8b3af723778d88359d8
YagaTG <ygatogo@gmail.com>
2024-08-30 13:40:35 +0300

    add styles and type for functions at CanvasContextType

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { ZoomButtons } from "./components/Zoom/ZoomButtons";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { CanvasProvider } from "./context/CanvasContext";
+import { Canvas } from "./components/Canvas/Canvas";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <main className="container">
+      <CanvasProvider>
+        <Sidebar />
+        <Canvas />
+        <ZoomButtons />
+      </CanvasProvider>
+    </main>
+  );
 }
 
 export default App;
--- src/context/CanvasContext.tsx
+++ src/context/CanvasContext.tsx
@@ -1,14 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, FC, PropsWithChildren, useState } from "react";
 import { MyCanvas } from "../types/CanvasType";
 
 type CanvasContextType = {
   currentZoom: number;
+  setCurrentZoom: React.Dispatch<React.SetStateAction<number>>;
   canvas: MyCanvas;
+  setCanvas: React.Dispatch<React.SetStateAction<MyCanvas>>;
 };
 
 export const CanvasContext = createContext<CanvasContextType | null>(null);
 
-export const CanvasProvider = ({ children }) => {
+export const CanvasProvider: FC<PropsWithChildren> = ({ children }) => {
   const [currentZoom, setCurrentZoom] = useState<number>(1);
   const [canvas, setCanvas] = useState<MyCanvas>(null);
 
