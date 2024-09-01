b16d4b59ec8c1ecbc0e9683fd3ec8544303db323
YagaTG <ygatogo@gmail.com>
2024-09-01 11:21:34 +0300

    add new styles and add history buttons to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { ZoomButtons } from "./components/Zoom/ZoomButtons";
 import { Sidebar } from "./components/Sidebar/Sidebar";
-import { CanvasProvider } from "./context/CanvasContext";
 import { Canvas } from "./components/Canvas/Canvas";
+import { ZoomButtons } from "./components/Zoom/ZoomButtons";
+import { CanvasProvider } from "./context/CanvasContext";
+import { HistoryButtons } from "./components/HistoryButtons/HistoryButtons";
 import "./App.css";
 
 function App() {
   return (
     <main className="container">
       <CanvasProvider>
+        <HistoryButtons />
         <Sidebar />
         <Canvas />
         <ZoomButtons />
